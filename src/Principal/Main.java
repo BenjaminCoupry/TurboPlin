87a3c82c7c10529a09e977dba19be3c8b96a1bbf
BenjaminCoupry <56168210+BenjaminCoupry@users.noreply.github.com>
2020-11-15 09:04:31 +0100

    Initial commit

---
+++ src/Principal/Main.java
@@ -0,0 +1,26 @@
+package Principal;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+
+public class Main extends JavaPlugin implements Listener {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return super.onCommand(sender, command, label, args);
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        this.getServer().getPluginManager().registerEvents(this,this);
+    }
+}
