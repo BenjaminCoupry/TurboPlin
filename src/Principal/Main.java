52b12238c2d5f79aa16470c150df1847ecf35ae3
BenjaminCoupry <56168210+BenjaminCoupry@users.noreply.github.com>
2020-11-15 10:19:15 +0100

    Zombies Dangereux

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -5,18 +5,25 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main extends JavaPlugin implements Listener {
+
+
 
     //Materiaux
     List<Material> tntOnly;
@@ -49,5 +56,43 @@ public class Main extends JavaPlugin implements Listener {
             event.setCancelled(true);
         }
 
+    }
+
+
+    @EventHandler
+    public void onEntityDamagedByEntity(EntityDamageByEntityEvent event) {
+
+        if (event.getDamager() instanceof Zombie) {
+            if (event.getEntity() instanceof Player) {
+                // Joueur tapé par zombie
+                Player p = (Player) event.getEntity();
+                Zombie z = (Zombie) event.getDamager();
+                PotionEffect ep = new PotionEffect(PotionEffectType.POISON, 7 * 20, 2);
+                PotionEffect eh = new PotionEffect(PotionEffectType.HUNGER, 30 * 20, 2);
+                PotionEffect ej = new PotionEffect(PotionEffectType.JUMP, 4 * 20, 2);
+                PotionEffect el = new PotionEffect(PotionEffectType.LEVITATION, 4 * 20, 2);
+                ep.apply(p);eh.apply(p);ej.apply(p);el.apply(p);
+                if (p.getHealth() - event.getDamage() <= 0.5) {
+                    //Joueur Meurt
+                    Zombie zp = (Zombie)z.getWorld().spawnEntity(p.getLocation(),EntityType.ZOMBIE);
+                    zp.getEquipment().setArmorContents(p.getEquipment().getArmorContents().clone());
+                    zp.setCustomName("(Z)"+p.getCustomName());
+                    zp.setCustomNameVisible(true);
+                }
+            }
+        }
+        if (event.getDamager() instanceof Player) {
+            if (event.getEntity() instanceof Zombie) {
+                //Zombie tapé par Joueur
+                Player p = (Player) event.getDamager();
+                Zombie z = (Zombie) event.getEntity();
+                PotionEffect eb = new PotionEffect(PotionEffectType.BLINDNESS, 3 * 20, 1);
+                eb.apply(p);
+                if (z.getHealth() - event.getDamage() <= 0.5) {
+                    //Zombie Meurt
+                    z.getWorld().createExplosion(z.getLocation(), 3);
+                }
+            }
+        }
     }
 }
